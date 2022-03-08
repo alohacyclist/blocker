a6a56db8ba290a3ba1bc36a83ab5279369b550b2
elmehdi rhindi <g6handi@gmail.com>
2022-03-08 18:28:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import "./styles.css";
-// We import other components to be able to use them inside of this component
-import Navbar from "./components/Navbar";
-import Button from "./components/Button";
-import CustomButton from "./components/CustomButton";
-import Headline from "./components/Headline";
-import Gallery from "./components/Gallery";
-
-// React Component <App />
-
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Headline />
-
-      <Button />
-      <CustomButton btnText="REACT NATIVE" url="https://reactjs.org" />
-      <CustomButton btnText="REDUX" url="hhttps://redux.js.org/" />
-
-      <Gallery />
+    <div>
+      Hello world
     </div>
   );
 }
