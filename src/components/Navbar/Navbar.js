73eb0f25928cf7ed5623eb26b44d91e4e21ebd0c
alohacyclist <c-mueller87@live.de>
2022-05-02 17:39:19 +0200

    add active class to nav buttons -css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,6 +10,13 @@ export function Navbar() {
 
   const { setLogin, setUser, setSearchPlaceholder, user, setSearchStyle, navigate } = useContext(CryptoContext)
 
+  const [activeWatchlistBtn, setActiveWatchlistBtn] = useState(false)
+  const [activeSearchBtn, setActiveSearchBtn] = useState(false)
+  const [activeUserWatchlistBtn, setActiveUserWatchlistBtn] = useState(false)
+  const [activeProfileBtn, setActiveProfileBtn] = useState(false)
+  const [activeLoginBtn, setActiveLoginBtn] = useState(false)
+  const [activeHomeBtn, setActiveHomeBtn] = useState(true)
+
   const handleLoginForm = (e) => {
     e.preventDefault()
     setLogin(true)
@@ -26,25 +33,93 @@ export function Navbar() {
     navigate('/')
   }
 
+  const handleSearchStyle = (e) => {
+    e.preventDefault()
+    setSearchStyle(styles.coin_search_show)
+    setSearchPlaceholder('Type to search for coins...')
+    window.scrollTo(0, 0)
+    navigate('/')
+  }
+
+  const handleHomeBtn = (e) => {
+    e.preventDefault()
+    setActiveHomeBtn('nav_btn_active')
+    setActiveSearchBtn(false)
+    setActiveUserWatchlistBtn(false)
+    setActiveProfileBtn(false)
+    setActiveLoginBtn(false)
+    setActiveWatchlistBtn(false)
+  }
+
+  const handleUserWatchlistBtn = (e) => {
+    e.preventDefault()
+    setActiveHomeBtn(false)
+    setActiveSearchBtn(false)
+    setActiveUserWatchlistBtn('nav_btn_active')
+    setActiveProfileBtn(false)
+    setActiveLoginBtn(false)
+    setActiveWatchlistBtn(false)
+  }
+
+  const handleProfileBtn = (e) => {
+    e.preventDefault()
+    setActiveHomeBtn(false)
+    setActiveSearchBtn(false)
+    setActiveUserWatchlistBtn(false)
+    setActiveProfileBtn('nav_btn_active')
+    setActiveLoginBtn(false)
+    setActiveWatchlistBtn(false)
+  }
+
+  const handleLoginBtn = (e) => {
+    e.preventDefault()
+    setActiveHomeBtn(false)
+    setActiveSearchBtn(false)
+    setActiveUserWatchlistBtn(false)
+    setActiveProfileBtn(false)
+    setActiveLoginBtn('nav_btn_active')
+    setActiveWatchlistBtn(false)
+  }
+
+  const handleSearchBtn = (e) => {
+    e.preventDefault()
+    setActiveHomeBtn(false)
+    setActiveSearchBtn('nav_btn_active')
+    setActiveUserWatchlistBtn(false)
+    setActiveProfileBtn(false)
+    setActiveLoginBtn(false)
+    setActiveWatchlistBtn(false)
+  }
+
+  const handleWatchlistBtn = (e) => {
+    e.preventDefault()
+    setActiveHomeBtn(false)
+    setActiveSearchBtn(false)
+    setActiveUserWatchlistBtn(false)
+    setActiveProfileBtn(false)
+    setActiveLoginBtn(false)
+    setActiveWatchlistBtn('nav_btn_active')
+  }
+
   return (
     <div>
       <div className='header'>
         <img className='logo' src={logo} style={{height: '3rem', margin: '5px'}}/>
         <div className='header_login_profile'>
         {user ? 
-          <button className='nav_btn' onClick={(e) => navigate('/user/profile')}><HiOutlineUser/></button> : 
-          <button className='nav_btn' onClick={(e) => {handleLoginForm(e)}}><FiUnlock/></button>}
+          <button className={activeProfileBtn ? 'nav_btn_active' : 'nav_btn'} onClick={(e) => {navigate('/user/profile'), handleProfileBtn(e)}}><HiOutlineUser/></button> : 
+          <button className={activeLoginBtn ? 'nav_btn_active' : 'nav_btn'} onClick={(e) => {handleLoginForm(e), handleLoginBtn(e)}}><FiUnlock/></button>}
         </div>
       </div>
       <nav>
         <div className='nav-icons-container'>
-            <Link to='/'><button className='nav_btn'><FiHome/></button></Link>
-            <Link to='/watchlist'><button className='nav_btn'><CgUserList/></button></Link>
+            <Link to='/'><button className={activeHomeBtn ? 'nav_btn_active' : 'nav_btn'} onClick={(e) => handleHomeBtn(e)}><FiHome/></button></Link>
+            <Link to='/watchlist'><button className={activeUserWatchlistBtn ? 'nav_btn_active' : 'nav_btn'}  onClick={(e) => handleUserWatchlistBtn(e)}><CgUserList/></button></Link>
         
-            <button className='nav_btn' onClick={() => {/* refactor to handlesearchstyle */setSearchStyle(styles.coin_search_show), setSearchPlaceholder('Type to search for coins...'), window.scrollTo(0, 0), navigate('/') }}><VscSearch /></button>
+            <button className={activeSearchBtn ? 'nav_btn_active' : 'nav_btn'} onClick={(e) => {handleSearchStyle(e), handleSearchBtn(e)}}><VscSearch /></button>
 
             {user ? 
-              <Link to='/user/watchlist'><button onClick={() => {window.scrollTo(0, 0)}} className='nav_btn'><IoGlassesOutline/></button></Link> : null}
+              <Link to='/user/watchlist'><button onClick={(e) => {window.scrollTo(0, 0), handleWatchlistBtn(e)}} className={activeWatchlistBtn ? 'nav_btn_active' : 'nav_btn'} ><IoGlassesOutline/></button></Link> : null}
             {user ? 
               <button className='nav_btn' onClick={() => {logout()}} ><MdOutlineLogout/></button> : null}
         </div>
